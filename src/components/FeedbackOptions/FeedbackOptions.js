138655aece6bc18290fc87790f3bb03d961f251c
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-12-14 18:06:54 +0200

    finsl

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -5,7 +5,18 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     <OptionsContainer>
       <Title>Please leave feedback</Title>
       {options.map(option => (
-        <Button key={option} onClick={onLeaveFeedback} name={option}>
+        <Button
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+          name={option}
+          style={
+            option === 'bad'
+              ? { backgroundColor: 'red' }
+              : option === 'neutral'
+              ? { backgroundColor: 'yellow' }
+              : {}
+          }
+        >
           {option}
         </Button>
       ))}
